c8cbf6a4865ac50d8eadf3935d61635168798368
M-Saim786 <saimrashid3344@gmail.com>
2023-04-02 01:17:27 +0500

    2/04/2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import { BiCommentAdd } from 'react-icons/bi'
+import { HiOutlineViewGridAdd } from 'react-icons/hi'
+import { AiFillDelete } from 'react-icons/ai'
+import { GrDocumentVerified, GrUpdate } from 'react-icons/gr'
+import './App.css'
 // idhr component 'extends' k zariye sa React(class) k ander se components k attributes lekr ai ga or App.jsx mein rkh de ga 
 export default class App extends Component {
   // 
@@ -13,46 +16,42 @@ export default class App extends Component {
     }
   }
   handle = (val) => {
-    //  console.log('click')
     // console.log(val)
     this.setState({ value: val })
-    // console.log(this.state.value)
   }
 
   submit = () => {
-    let obj = {
-      title: this.state.value,
-      todo_state: 0
+    if (this.state.value == '') {
+      alert('Add Todo First...!')
     }
-    // here we use spread operator 
-    this.state.todo = [...this.state.todo, obj]
-
-    localStorage.setItem("Todos", JSON.stringify(this.state.todo))
-    this.setState({
-      value: ''
-    })
-    // console.log(this.state.todo)
-    // console.log(this.state.todo.length)
+    else {
+      let obj = {
+        title: this.state.value,
+        todo_state: 0
+      }
+      // here we use spread operator 
+      this.state.todo = [...this.state.todo, obj]
+      localStorage.setItem("Todos", JSON.stringify(this.state.todo))
+      this.setState({
+        value: ''
+      })
+    }
   }
+
   deletefunc = (e) => {
-
-    // console.log(e)
-    // console.log(this.state.todo)
     this.state.todo.splice(e, 1)
-
     localStorage.setItem("Todos", JSON.stringify(this.state.todo))
     window.location.reload()
-
-    // this.setState({
-    // })
   }
+
+
+  
 
   update = (e) => {
     console.log(e)
     for (let i = 0; i < this.state.todo.length; i++) {
       this.state.todo[i].todo_state = 0
     }
-
     this.state.todo[e].todo_state = 1
     this.setState({})
     console.log(this.state.todo[e])
@@ -63,65 +62,79 @@ export default class App extends Component {
     console.log(ind)
     this.state.todo[ind].title = ch.target.value
     this.setState({})
-
   }
   save = (index) => {
     console.log('save')
     this.state.todo[index].todo_state = 0
     localStorage.setItem("Todos", JSON.stringify(this.state.todo))
     this.setState({})
-
   }
 
-
   componentDidMount() {
-
     let get_data = localStorage.getItem("Todos")
     get_data == null ? this.state.todo = [] : this.state.todo = JSON.parse(get_data)
     this.setState({})
 
+// this.GetStart()
   }
+
+  GetStart = () => {
+    document.getElementsByClassName("container")[0].classList.add('active')
+    document.getElementsByClassName("getStart")[0].style.display = 'none'
+    document.getElementById("mainDiv").style.display = 'block'
+  
+  }
+
   render() {
     return (
       <>
-        <h2>todo list</h2>
-
-        <input type="text" name="" id="" onChange={(e) => this.handle(e.target.value)} />
-
-        <button onClick={() => this.submit()}>
-          <BiCommentAdd size={20} />
-        </button>
-        <ol>
-          {
-            this.state.todo == '' ? <li>No Data to Show</li> :
-              this.state.todo.map((v, i) => {
-                return (
-                  v.todo_state === 0 ?
-                    <>
-                      <li key={i}>
-                        {v.title}
-
-                        <button onClick={() => this.deletefunc(i)}>
-                          Delete
-                        </button>
-                        <button onClick={() => this.update(i)}>
-                          Update
-                        </button>
-                      </li>
-                    </>
-                    :
-                    <li key={i}>
-                      <input type="text" name="" id="" value={v.title} onChange={(ch) => this.updateChange(ch, i)} />
-                      <button onClick={() => this.save(i)}>Save </button>
-                    </li>
-
-                )
-
-              })
-          }
-        </ol>
+        <div className="container">
+          <div className="getStart">
+            <button onClick={this.GetStart}>
+              Let's Start
+            </button>
+          </div>
+          {                           /* Main Div  */}
+          <div id='mainDiv' >
+            <h1>Todo list</h1>
+            <input type="text" name="" id="AddTodo" onChange={(e) => this.handle(e.target.value)} />
+            <button id='submit' onClick={() => this.submit()} >
+              <HiOutlineViewGridAdd />
+            </button>
+            <ol>
+              {
+                this.state.todo == '' ? <div id='noData'><h3>
+                  No Data to Show</h3></div> :
+                  this.state.todo.map((v, i) => {
+                    return (
+                      v.todo_state === 0 ?
+                        <>
+                          <li key={i}>
+                            {v.title}
+                            <div className="btn">
+                              <button className='btns' onClick={() => this.update(i)}>
+                                <GrUpdate />
+                              </button>
+                              <button className='btns' id='deleteBtn' onClick={() => this.deletefunc(i)}>
+                                <AiFillDelete size={20} />
+                              </button>
+                            </div>
+                          </li>
+                        </>
+                        :
+                        <li key={i}>
+                          <input type="text" name="" id="" value={v.title} onChange={(ch) => this.updateChange(ch, i)} />
+                          <div className="btn" style={{ 'margin-top': '-0.5rem' }}>
+                            <button id='saveBtn' className='btns' onClick={() => this.save(i)}>Save </button>
+                          </div>
+                        </li>
+                    )
+                  })
+              }
+            </ol>
+          </div>
+        </div>
       </>
     )
   }
-
 }
